3a8eea70f2d7a19fe124a90ce6056134cc993801
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-10-22 01:28:26 -0400

    add search function

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -6,6 +6,7 @@ import Search from "./Search";
 function PlantPage() {
 
   const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:6001/plants')
@@ -21,17 +22,24 @@ function PlantPage() {
   }
   
 
-const plantsToDisplay = plants
+  const displayedPlants = plants.filter((plant) => {
+    return (
+      plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  })
 
   return (
     <main>
       <NewPlantForm 
-        plants = {plantsToDisplay} 
+        plants = {displayedPlants} 
         onAddPlant = {handleAddPlant}  
       />
-      <Search />
+      <Search 
+         searchTerm = {searchTerm}
+         onSearchChange = {setSearchTerm} 
+      />
       <PlantList 
-        plants = {plantsToDisplay} 
+        plants = {displayedPlants} 
         
       />
     </main>
